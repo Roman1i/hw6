eb817ce1188baba557d0e823c61c80e481cbf720
Roman <igoshinr.estho@gmail.com>
2022-08-14 17:31:22 +0300

    41

---
+++ 41/Program.cs
@@ -0,0 +1,9 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[m];
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if (arr[i] > 0) count++;
+}
+Console.WriteLine(count);
