fa31b269937a3cdfc6ac95a5afd33f05e446ed92
Roman <igoshinr.estho@gmail.com>
2022-08-16 13:23:10 +0300

    transport

---
+++ transport/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[,] arr = new int[n,n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("");
+    for (int j = 0; j < n; j++)
+    {
+        arr[i, j] = new Random().Next(0,10);
+        Console.Write(arr[i, j] + " ");
+    }   
+}
+
+Console.WriteLine("");
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("");
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(arr[j, i] + " ");
+    }
+}
